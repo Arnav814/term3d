5e4e5fb2bc2d40a8084d9c1a7984f4c1ea643ca6
Arnav495 <arnieincyberland@gmail.com>
2025-02-18 11:37:34 -0800

    Improve glm::vec formatter

--- src/util/formatters.hpp
+++ src/util/formatters.hpp
@@ -2,9 +2,9 @@
 #define GLMFORMATTERS_HPP
 #include "glm/detail/qualifier.hpp"
 #include "glm/ext/vector_double3.hpp"
-#include <print>
 #include <format>
 #include <iostream>
+#include <print>
 #include <vector>
 
 template <glm::length_t Length, typename VecType, glm::qualifier Qual>
@@ -15,10 +15,19 @@ struct std::formatter<glm::vec<Length, VecType, Qual>> : std::formatter<VecType>
 		// can't use glm::to_string() here because it's not consteval :(
 		auto out = ctx.out();
 		out = std::format_to(out, "{}vec{}(", typeid(VecType).name()[0], Length);
-		if constexpr (Length >= 1) out = std::format_to(out, "{}", val.x);
-		if constexpr (Length >= 2) out = std::format_to(out, ", {}", val.y);
-		if constexpr (Length >= 3) out = std::format_to(out, ", {}", val.z);
-		if constexpr (Length >= 4) out = std::format_to(out, ", {}", val.w);
+		if constexpr (Length >= 1) std::formatter<VecType>::format(val.x, ctx);
+		if constexpr (Length >= 2) {
+			out = std::format_to(out, ", ");
+			std::formatter<VecType>::format(val.y, ctx);
+		}
+		if constexpr (Length >= 3) {
+			out = std::format_to(out, ", ");
+			std::formatter<VecType>::format(val.z, ctx);
+		}
+		if constexpr (Length >= 4) {
+			out = std::format_to(out, ", ");
+			std::formatter<VecType>::format(val.w, ctx);
+		}
 		out = std::format_to(out, ")");
 		ctx.advance_to(out);
 		return out;
